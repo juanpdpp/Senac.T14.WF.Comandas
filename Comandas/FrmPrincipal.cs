31fcdf97e9892effd704a22c39f61161a1c320bd
juanpdpp <164434341+juanpdpp@users.noreply.github.com>
2024-05-23 20:09:31 -0300

    projeto criado com um conjunto de quatro botoes na groupbox. Initial commit

---
+++ Comandas/FrmPrincipal.cs
@@ -0,0 +1,15 @@
+namespace Comandas
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
